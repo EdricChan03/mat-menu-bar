d446ff438ebb6e7f33c42acad1e4e424ff513d6c
Edric <edric.chan.1997@gmail.com>
2020-02-04 02:20:43 +0800

    feat(demos): add support for custom demo icon

--- projects/demo/src/app/demos.service.ts
+++ projects/demo/src/app/demos.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 
+export interface Demo {
+  title: string;
+  id: string;
+  icon?: string;
+  svgIcon?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class DemosService {
   /** The list of demos. */
-  readonly demos = [
+  readonly demos: Demo[] = [
     {
       title: 'Docs app menu bar',
-      id: 'docs-app-menu-bar'
+      id: 'docs-app-menu-bar',
+      svgIcon: 'mdi:file-document-box'
     },
     {
       title: 'Custom menu bar',
